fecf3a9d423bd8f5d82dc4eb37e06debb8670929
Luy Felipe <57736733+luyfgomes@users.noreply.github.com>
2021-10-07 00:46:17 -0300

    Atualização
    Melhorias na validação do CPF e adição de interface gráfica (PySimpleGUI)

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
+# -*- coding: utf-8 -*-
 import re
+import PySimpleGUI as sg
 
-texto = input('Formatos:\n- CPF: 111.222.333-44: \n- E-mail: asvc@...: \n- Placa de veículo: xxx/XXX-123 \nEntrada: ')
+layout = [[sg.Text("Digite uma das informações abaixo para saber se é valido:")],
+          [sg.Text("Formatos:\n -CPF: 111.222.333-44: \n -E-mail: asvc@...: \n -Placa de veículo: xxx/XXX-1234 \n")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Text(size=(40, 1), key='-OUTPUT-')],
+          [sg.Button("Validar"), sg.Button("Sair")]]
 
-formatoCPF = re.compile(r'([\d]{3}\.){2}[\d]{3}-[\d]{2}') #Formato CPF
-formatoEmail = re.compile(r'^[\w._-]+@[\w]+\.[\a-z]') #Formato e-mail
-formatoPlaca = re.compile(r'^[a-zA-Z]{3}-\d{4}') #Formato placa
+# Definição dos formatos da entrada para combinar com CPF, e-mail e placa
+formatoCPF = re.compile(r'([\d]{3}\.){2}[\d]{3}-[\d]{2}')
+formatoEmail = re.compile(r'^[\w._-]+@[\w]+.[\a-z]{4}')
+formatoPlaca = re.compile(r'^[a-zA-Z]{3}-\d{4}')
 
-#Se a entrada combinar com formato CPF
-if formatoCPF.match(texto):
-    CPFsemPonto = re.sub(r'[^\w\s]', '', texto) #Retira os pontos do CPF para manipulação
-    CPFsemNum = CPFsemPonto[:-2] #Retira os 2 ultimos digitos para verificar
+window = sg.Window("Teste de Validação", layout)
 
-#Calculo para validar os digitos
-    digitoUm = 0
-    digitoDois = 0
-    multiplicador = 10
-    contador = 0
-    for i in CPFsemNum: #Calculo para descobrir o primeiro digito
-        contador += int(i) * multiplicador
-        multiplicador -= 1
-    modDivisao = contador % 11
-    if modDivisao <= 10 and modDivisao> 0:
-        digitoUm = 11 - modDivisao
-    CPFsemNum += str(digitoUm)
-    multiplicador = 11
-    contador = 0
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == "Sair":
+        break
 
-    for i in CPFsemNum: #Calculo para descobrir o segundo digito
-        contador += int(i) * multiplicador
-        multiplicador -= 1
-    modDivisao = contador % 11
-    if modDivisao <= 10 and modDivisao> 0:
-        digitoDois = 11 - modDivisao
-    CPFsemNum += str(digitoDois)
+    # Se a entrada combinar com formato CPF
+    if formatoCPF.match(values['-INPUT-']):
+        CPFsemPonto = re.sub(r'[^\w\s]', '', values['-INPUT-'])  # Retira os pontos do CPF para manipulação
+        CPFsemNum = CPFsemPonto[:-2]  # Retira os 2 ultimos digitos para verificar
 
-#Compara os dois ultimos digitos do CPF informado e do calculado
-    if CPFsemNum[-2::] == texto[-2::]:
-        print("É um CPF válido")
+        # Calculo para validar os digitos
+        digitoUm = 0
+        digitoDois = 0
+        multiplicador = 10
+        contador = 0
+        for i in CPFsemNum:  # Calculo para descobrir o primeiro digito
+            contador += int(i) * multiplicador
+            multiplicador -= 1
+        modDivisao = contador % 11
+        if 10 >= modDivisao > 0:
+            digitoUm = 11 - modDivisao
+        CPFsemNum += str(digitoUm)
+        multiplicador = 11
+        contador = 0
+
+        for i in CPFsemNum:  # Calculo para descobrir o segundo digito
+            contador += int(i) * multiplicador
+            multiplicador -= 1
+        modDivisao = contador % 11
+        if 10 >= modDivisao > 0:
+            digitoDois = 11 - modDivisao
+        CPFsemNum += str(digitoDois)
+
+        if CPFsemNum[-2::] == values['-INPUT-'][-2::]:
+            window['-OUTPUT-'].update(values['-INPUT-'] + "\tCPF Valido", text_color='yellow')
+        else:
+            window['-OUTPUT-'].update(values['-INPUT-'] + "\tCPF Invalido", text_color='red')
+
+    elif formatoEmail.match(values['-INPUT-']):
+        window['-OUTPUT-'].update(values['-INPUT-'] + "\tE-mail Valido", text_color='yellow')
+    elif formatoPlaca.match(values['-INPUT-']):
+        window['-OUTPUT-'].update(values['-INPUT-'] + "\t Placa Valida", text_color='yellow')
     else:
-        print('Não é um CPF válido')
+        window['-OUTPUT-'].update(values['-INPUT-'] + "\t Formato Invalido", text_color='red')
 
-#Se a entrada combinar com formato e-mail
-elif formatoEmail.match(texto):
-    print("É um e-mail válido")
-#Se a entrada combinar com formato Placa
-elif formatoPlaca.match(texto):
-    print("É um formato de placa válido")
-#Se não combinar com nenhum formato
-else:
-    print('Não é um formato válido')
+window.close()
